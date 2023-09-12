3f759604a54647711a862a00f328cf70e8d1376e
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2023-09-11 21:06:35 -0300

    calculadora

---
+++ AULA04/exextras/calculadora/script.js
@@ -0,0 +1,21 @@
+var resultado = document.querySelector('h1#res')
+var n1 = ''
+
+function num1() {
+    n1 += '1'
+    resultado.innerText = n1
+}
+
+function num2() {
+    n1 += '2'
+    resultado.innerText = n1
+}
+
+function num3() {
+    n1 += '3'
+    resultado.innerText = n1
+}
+
+function somar(){
+    
+}
