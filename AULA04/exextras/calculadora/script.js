1347f0f8aa03fe569c8c613a4e2fa3f785768923
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2023-09-13 08:59:28 -0300

    criar a função soma e calcular

--- AULA04/exextras/calculadora/script.js
+++ AULA04/exextras/calculadora/script.js
@@ -1,4 +1,8 @@
 var resultado = document.querySelector('h1#res')
+var test1 = document.querySelector('h1#v1')
+var test2 = document.querySelector('h1#v2')
+var resp = document.querySelector('h1#r')
+var calc = document.querySelector('button#calc')
 var n1 = ''
 let n2 = ''
 
@@ -18,5 +22,16 @@ function num3() {
 }
 
 function somar(){
-    
+    var v1 = Object.assign(Number(n1))
+    n1 = ''
+    test1.innerText = v1
+}
+
+function calcular(){
+    var v2 = Object.assign(Number(n1))
+    test2.innerText = v2
+    let soma = v1.value + v2.value  
+    resp.innerText = soma
+
+
 }
