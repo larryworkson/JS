793d48d1f6b860f37b570fea1417d3ae798de2af
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2023-09-14 10:32:50 -0300

    v1.1 adicionei histórico de calculos

--- AULA04/exextras/calculadora/script.js
+++ AULA04/exextras/calculadora/script.js
@@ -1,11 +1,12 @@
-var resultado = document.querySelector('th#res')
+var display = document.querySelector('th#res')
 var n1 = []
 let n2 = []
+var hist = [] /* histórico de calculos, para usar resultados em outras operações */
 var op = '' /* operador aritmetico */
 var troca_num = new Boolean (true) /* muda a lista para qual o número será adicionado. Este boolean define se é o primeiro ou segundo valor que o usuário inseriu. Se for o primeiro, ele vai para n1 se for o segundo vai para n2 */
 
 
-resultado.innerText = ''
+display.innerText = ''
 
 
 function addnum(num){
@@ -15,13 +16,13 @@ function addnum(num){
     else {
         n2.push(num)
     }
-    resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
+    display.innerText = `${n1.join('')}${op}${n2.join('')}`
 }
 
 function somar(){
     troca_num = false
     op = '+'
-    resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
+    display.innerText = `${n1.join('')}${op}${n2.join('')}`
 
 }
 
@@ -30,17 +31,18 @@ function calcular(){
         let v1 = n1.join('')
         let v2 = n2.join('')
         var s = Number(v1) + Number(v2)
-        resultado.innerText = ''
+        display.innerText = ''
         op = ''
-        resultado.innerText = s
-        n1 = []
+        hist.push(s) /* adiciona o resultado da soma (s) no histórico */
+        display.innerText = hist.slice(-1) /* apresenta o último item adicionado no histórico */
+        n1 = [] /* limpa a lista */
         n2 = []
-        troca_num = true
+        n1.push(s) /* considera o resultado do último cálculo como primeiro valor de uma nova operação */
     }
 }
 
 function zerar() {
-    resultado.innerText = '0'
+    display.innerText = '0'
     op = ''
     n1 = []
     n2 = []
