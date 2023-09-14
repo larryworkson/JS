6d2cd6d6618f7d3910f67dd176a7095f6470d922
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2023-09-14 00:44:25 -0300

    ta ficando fera!

--- AULA04/exextras/calculadora/script.js
+++ AULA04/exextras/calculadora/script.js
@@ -1,51 +1,63 @@
-var resultado = document.querySelector('h1#res')
-var test1 = document.querySelector('h1#v1')
-var test2 = document.querySelector('h1#v2')
-var resp = document.querySelector('h1#r')
+var resultado = document.querySelector('th#res')
 var calc = document.querySelector('button#calc')
 var n1 = []
 let n2 = []
+var op = ''
 var lado = new Boolean (true)
 var adicao = new Boolean (false) /* esta variável só é ativada se o usuário clicar em + */
 
+resultado.innerText = ''
 /* criei um boolean para saber se é o primeiro ou segundo valor que o usuário entrou. Se for o primeiro, ele vai para n1 se for o segundo vai para n2 */
+function num0() {
+    if (lado == true) {
+        n1.push(0)
+        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
+    }
+    else {
+        n2.push(0)
+        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
+        
+    }
+    }
+
 function num1() {
     if (lado == true){    
         n1.push(1)
-        resultado.innerText += n1.slice(-1) /* pega o último valor do array */
+        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
     }
     else {
         n2.push(1)
-        resultado.innerText += n2.slice(-1)
+        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
     }
 }
 
 function num2() {
     if (lado == true) {
         n1.push(2)
-        resultado.innerText += n1.slice(-1)
+        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
     }
     else {
         n2.push(2)
-        resultado.innerText += n2.slice(-1)
+        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
     }
 }
 
 function num3() {
     if (lado == true) {
         n1.push(3)
-        resultado.innerText += n1.slice(-1)
+        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
     }
     else {
         n2.push(3)
-        resultado.innerText += n2.slice(-1)
+        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
     }
 }
 
 function somar(){
     lado = false
     adicao = true
-    resultado.innerText += '+'
+    op = '+'
+    resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
 
 }
 
@@ -55,13 +67,19 @@ function calcular(){
         let v2 = n2.join('')
         var s = Number(v1) + Number(v2)
         resultado.innerText = ''
+        op = ''
         resultado.innerText = s
         n1 = []
         n2 = []
+        lado = true
+       
 
     }
 }
 
 function zerar() {
-    resultado.innerText = ''
+    resultado.innerText = '0'
+    op = ''
+    n1 = []
+    n2 = []
 }
