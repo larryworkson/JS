259f5c9e6972513fbe4edc35b7655771cb2e2c6d
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2023-09-14 10:03:10 -0300

    V1 funcionando
    adição de numeros inteiros 100% funcionando.

--- AULA04/exextras/calculadora/script.js
+++ AULA04/exextras/calculadora/script.js
@@ -4,65 +4,30 @@ var n1 = []
 let n2 = []
 var op = ''
 var lado = new Boolean (true)
-var adicao = new Boolean (false) /* esta variável só é ativada se o usuário clicar em + */
+
 
 resultado.innerText = ''
 /* criei um boolean para saber se é o primeiro ou segundo valor que o usuário entrou. Se for o primeiro, ele vai para n1 se for o segundo vai para n2 */
-function num0() {
-    if (lado == true) {
-        n1.push(0)
-        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
-    }
-    else {
-        n2.push(0)
-        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
-        
-    }
-    }
 
-function num1() {
-    if (lado == true){    
-        n1.push(1)
-        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
-    }
-    else {
-        n2.push(1)
-        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
-    }
-}
-
-function num2() {
+function addnum(num){
     if (lado == true) {
-        n1.push(2)
-        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
+        n1.push(num)
     }
     else {
-        n2.push(2)
-        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
+        n2.push(num)
     }
-}
-
-function num3() {
-    if (lado == true) {
-        n1.push(3)
-        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
-    }
-    else {
-        n2.push(3)
-        resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
-    }
+    resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
 }
 
 function somar(){
     lado = false
-    adicao = true
     op = '+'
     resultado.innerText = `${n1.join('')}${op}${n2.join('')}`
 
 }
 
 function calcular(){    
-    if (adicao == true) {
+    if (op == '+') {
         let v1 = n1.join('')
         let v2 = n2.join('')
         var s = Number(v1) + Number(v2)
@@ -72,8 +37,6 @@ function calcular(){
         n1 = []
         n2 = []
         lado = true
-       
-
     }
 }
 
