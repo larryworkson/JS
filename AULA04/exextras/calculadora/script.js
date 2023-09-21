2a373e2b0de90a86a35c32ad5c773369a6bfd31f
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2023-09-21 19:52:04 -0300

    v1.4 bug de histórico corrigido

--- AULA04/exextras/calculadora/script.js
+++ AULA04/exextras/calculadora/script.js
@@ -6,9 +6,14 @@ let n2 = []
 var hist = [] /* histórico de calculos, para usar resultados em outras operações */
 var op = '' /* operador aritmetico */
 var troca_num = new Boolean (true) /* muda a lista para qual o número será adicionado. Este boolean define se é o primeiro ou segundo valor que o usuário inseriu. Se for o primeiro, ele vai para n1 se for o segundo vai para n2 */
+var igual = false /* torna-se true quando usuário clica em = */
 display.innerText = '0'
 
 function addnum(num){
+    if (igual == true && op == '') { /* se usuário já clicou em = e não adicionou um operador (op), significa que ele não está deseja usar o histórico. Então a n1 é zerada, para zerar o display */
+        n1 = []
+        igual = false /* ao zerar a n1, a var igual tbm volta ao estado original (false) */
+    }
     if (troca_num == true) {
         n1.push(num)
     }
@@ -30,33 +35,37 @@ function operar(operador){
 
 }
 
-function calcular(){    
+function calcular(){   
+    igual = true 
     if (op == '+') {
         let v1 = n1.join('')
         let v2 = n2.join('')
         var s = Number(v1) + Number(v2)
         display.innerText = ''
         op = ''
-        if (Number(s) === s && s % 1 !== 0){
+        if (Number(s) === s && s % 1 !== 0){ /* se o resultado for um Float, deve terá apenas 2 casas decimais */
             hist.push(s.toFixed(2))
         }
         else {
             hist.push(s)
         }
          /* adiciona o resultado da soma (s) no histórico */
-        display.innerText = hist.slice(-1) /* apresenta o último item adicionado no histórico arredondado*/
+        display.innerText = hist.slice(-1) /* apresenta o último item adicionado no histórico*/
         n1 = [] /* limpa a lista */
         n2 = []
         n1.push(s) /* considera o resultado do último cálculo como primeiro valor de uma nova operação */
+        troca_num = true /* volta a considerar a n1 para receber os dados */
     }
 }
 
 function zerar() {
     display.innerText = '0'
     troca_num = true
+    igual = false
     op = ''
     n1 = []
     n2 = []
+    hist = []
 }
 
 function deletar() {
