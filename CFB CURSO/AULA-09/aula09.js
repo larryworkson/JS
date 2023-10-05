69849fa29860405e4cab94153977d6808ca2477d
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2023-10-05 19:09:11 -0300

    curso js

---
+++ CFB CURSO/AULA-09/aula09.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let n=10
+
+++n // PRÉ INCREMENTO: altera o valor antes de exibí-lo
+n++ // PÓS INCREMENTO: altera o valor depois de exibí-lo
+let x = -n // inverte o sinal
+console.log(++n)
+console.log(x)
+
+var n1 = 10
+var n2 = 10
+console.log(n1 + n2)
