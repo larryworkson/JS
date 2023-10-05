6636e237943e7823e11d76eec9a313001ca02139
Larry Rocha <143459283+larryworkson@users.noreply.github.com>
2023-10-05 20:29:11 -0300

    Sistema Agora permite deletar um cadastro

--- AULA04/exextras/sistCadastros/script.js
+++ AULA04/exextras/sistCadastros/script.js
@@ -1,30 +1,36 @@
 "use strict"
 var base = []
 
+function del(id) {
+    base.splice(id, 1)
+    atualizarTab()
+}
+
+function atualizarTab() {
+    tot.innerText = base.length
+    tab.innerHTML = '' /* zerando a tabela para não duplicar os itens */
+    for (var c = 0 ; c <= base.length ; c++) {
+        var linha = document.createElement('tr')
+        linha.innerHTML = `<th>${c}<td>${base[c].nome} <td>${base[c].email}<td>${base[c].tel}<td>${base[c].cid} <td><button onclick="del(${c})">DEL</button>`        
+        tab.appendChild(linha)              
+    }
+    
+}
 
 function cadastrar() {
     var cadastros = {}
     var tabela = document.querySelector('table#tabela')
-    var nome = document.querySelector('input#nome').value
+    var nomePessoa = document.querySelector('input#nome').value
     var email = document.querySelector('input#email').value
     var tel = document.querySelector('input#tel').value
     var cidade = document.querySelector('input#cidade').value
     var tab = document.querySelector('tbody#tab')
     var tot = document.querySelector('td#tot')
     tabela.style.display = 'block'
-    cadastros.nome = nome
+    cadastros.nome = nomePessoa
     cadastros.email = email
     cadastros.tel = tel
     cadastros.cid = cidade
     base.push(cadastros)
-    tot.innerText = base.length
-    tab.innerHTML = '' /* zerando a tabela para não duplicar os itens */
-    for (var c = 0 ; c <= base.length ; c++) {
-        var linha = document.createElement('tr')
-        linha.innerHTML = `<td>${base[c].nome} <td>${base[c].email}<td>${base[c].tel}<td>${base[c].cid}`        
-        tab.appendChild(linha)              
-    }
-   
-
+    atualizarTab()
 }
-/* criar um ID e adicionar na tabela */
--- CFB CURSO/AULA-12-SPREAD/aula12.js
+++ CFB CURSO/AULA-12-SPREAD/aula12.js
@@ -1,6 +1,31 @@
-let n1 = [10, 20, 30]
+/* SPREAD ...
+Junta os elementos de arrays e objetos */
+
+/* let n1 = [10, 20, 30]
 let n2 = [11, 22, 33, 44]
-let n3 = n1
-console.log(n1)
-console.log(n2)
-console.log(n3)
+let n3 = [...n1]
+
+document.write(n3)
+document.write(typeof(n3)) */
+
+/* ÚTIL PARA SOMAR VALORES DE UM OBJETO */
+/*
+function somar(n1, n2, n3) {
+    return n1 + n2 + n3
+}
+
+let valores = [10, 3, 4]
+document.write(somar(...valores))
+*/
+
+
+/* ÚTIL PARA MANIPULAR UMA COLEÇÃO HTML  */
+
+const objetos1 = document.getElementsByTagName("div") // uma coleção HTML só recebe elementos HTML
+const objetos2 = [...document.getElementsByTagName("div")] // um Array pode receber outros tipos de elementos
+//document.write(objetos1)
+//document.write(objetos2)
+
+for (item in objetos2) { /* usando o SPREAD é possível iterar uma coleção HTML */
+    objetos2[item].innerHTML = 'teste'
+}
